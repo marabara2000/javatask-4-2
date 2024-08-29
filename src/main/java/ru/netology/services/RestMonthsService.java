1393ced779f53a899353c6a4dcd0c9e6e4b43e4b
Marina B <balda558@gmail.com>
2024-08-29 18:25:10 +0300

    Reformat Code

--- src/main/java/ru/netology/services/RestMonthsService.java
+++ src/main/java/ru/netology/services/RestMonthsService.java
@@ -5,7 +5,7 @@ public class RestMonthsService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-                for (int month = 1; month < 13; month++) {
+        for (int month = 1; month < 13; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
                 money = (money - expenses) / 3;
@@ -14,7 +14,5 @@ public class RestMonthsService {
             }
         }
         return count;
-
-
     }
 }
--- src/test/java/ru/netology/services/RestMonthsServiceTest.java
+++ src/test/java/ru/netology/services/RestMonthsServiceTest.java
@@ -1,44 +1,19 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestMonthsServiceTest {
 
     @ParameterizedTest
-//    @CsvSource( {
-//            "3, 10, 3, 20",
-//            "2, 100, 60, 150"
-//    })
-    @CsvFileSource(files = "src/test/resources/restmonths.csv")
-    public void testFirst (int expected, int income, int expenses, int threshold) {
+    @CsvFileSource(files = "src/test/resources/months.csv")
+    public void testFirst(int expected, int income, int expenses, int threshold) {
         RestMonthsService service = new RestMonthsService();
 
-//        int income = 10;
-//        int expenses = 3;
-//        int threshold = 20;
-
-       // int expected = 3;
-        int actual = service.calculate (income, expenses, threshold);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-//    @Test
-//    void testSecond () {
-//        RestMonthsService service = new RestMonthsService();
-//
-//        int income = 100;
-//        int expenses = 60;
-//        int threshold = 150;
-//
-//        int expected = 2;
-//        int actual = service.calculate (income, expenses, threshold);
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
-
 
 }
